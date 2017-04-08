24abb69b642b1bbc761f35e5dfeaf634a6222661
Christopher Dombele <Christopher Dombele>
2017-04-08 13:51:00 -0400

    Added code that will be modified

--- SlotGame/src/slotGame/SlotGame.java
+++ SlotGame/src/slotGame/SlotGame.java
@@ -1,10 +1,88 @@
 package slotGame;
 
-public class SlotGame {
+import java.util.ArrayList;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class SlotGame extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 1; i <= 13; i++) {
+			list.add(i);
+		}
+		
+		// Use the static shuffle method in the java.util.Collections class.
+		java.util.Collections.shuffle(list);
+
+		HBox hBox = new HBox(5);
+		hBox.setAlignment(Pos.CENTER);
+		hBox.setPadding(new Insets(5,0,0,0));
+		hBox.getChildren().add(new ImageView("image/card/" + list.get(0) + ".png"));
+		hBox.getChildren().add(new ImageView("image/card/" + list.get(1) + ".png"));
+		hBox.getChildren().add(new ImageView("image/card/" + list.get(2) + ".png"));
+		hBox.getChildren().add(new ImageView("image/card/" + list.get(3) + ".png"));
+		hBox.getChildren().add(new ImageView("image/card/" + list.get(4) + ".png"));
+		
+		// Create buttons
+		HBox hBtnBox = new HBox(10);
+		hBtnBox.setAlignment(Pos.CENTER);
+		hBtnBox.setPadding(new Insets(5,0,5,0));
+		
+		Button btRefresh = new Button("Refresh");
+		btRefresh.setOnAction(e -> {
+			java.util.Collections.shuffle(list);
+			
+			hBox.getChildren().clear();
+			hBox.getChildren().add(new ImageView("image/card/" + list.get(0) + ".png"));
+			hBox.getChildren().add(new ImageView("image/card/" + list.get(1) + ".png"));
+			hBox.getChildren().add(new ImageView("image/card/" + list.get(2) + ".png"));
+			hBox.getChildren().add(new ImageView("image/card/" + list.get(3) + ".png"));
+			hBox.getChildren().add(new ImageView("image/card/" + list.get(4) + ".png"));
+		});
+		
+		Button btSort = new Button("Sort");
+		btSort.setOnAction(e -> {
+			java.util.Collections.sort(list.subList(0, 5));
+			
+			hBox.getChildren().clear();
+			hBox.getChildren().add(new ImageView("image/card/" + list.get(0) + ".png"));
+			hBox.getChildren().add(new ImageView("image/card/" + list.get(1) + ".png"));
+			hBox.getChildren().add(new ImageView("image/card/" + list.get(2) + ".png"));
+			hBox.getChildren().add(new ImageView("image/card/" + list.get(3) + ".png"));
+			hBox.getChildren().add(new ImageView("image/card/" + list.get(4) + ".png"));
+		});
+		
+		hBtnBox.getChildren().addAll(btRefresh, btSort);
+		
+		BorderPane pane = new BorderPane();
+		pane.setCenter(hBox);
+		pane.setBottom(hBtnBox);
+		BorderPane.setAlignment(btRefresh, Pos.TOP_CENTER);
+
+		// Create a scene and place it in the stage
+		Scene scene = new Scene(pane, 400, 150);
+		primaryStage.setTitle("Lab10_Problem1"); // Set the stage title
+		primaryStage.setScene(scene); // Place the scene in the stage
+		primaryStage.show(); // Display the stage
+	}
+
+	/**
+	 * The main method is only needed for the IDE with limited JavaFX support.
+	 * Not needed for running from the command line.
+	 */
 	public static void main(String[] args) {
-		System.out.print("random6");
-
+		Application.launch(args);
 	}
 
 }
