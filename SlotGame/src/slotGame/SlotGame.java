628aad5823488ee5d867c1e175748fc1f9bf9762
Dombele <dombelec@wit.edu>
2017-04-09 18:29:36 -0400

    Removed images

--- SlotGame/src/slotGame/SlotGame.java
+++ SlotGame/src/slotGame/SlotGame.java
@@ -1,6 +1,7 @@
 package slotGame;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -13,14 +14,23 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class SlotGame extends Application {
-
+	Random rand = new Random();
 	@Override
 	public void start(Stage primaryStage) {
-		
+		int i;
 		ArrayList<Integer> list = new ArrayList<>();
-		for (int i = 1; i <= 13; i++) {
+		for (i = 1; i <= 6; i++) {
 			list.add(i);
+			
 		}
+		
+//		int value = rand.nextInt(list);
+		
+//		int n = new rand.nextInt(6)+1;
+//		int random0 = (int) (Math.random() * 6 + 1);
+//		int random1 = (int) (Math.random() * 7 + 1);
+//		int random2 = (int) (Math.random() * 5 + 1);
+		
 		
 		// Use the static shuffle method in the java.util.Collections class.
 		java.util.Collections.shuffle(list);
@@ -28,40 +38,44 @@ public class SlotGame extends Application {
 		HBox hBox = new HBox(50);
 		hBox.setAlignment(Pos.CENTER);
 		hBox.setPadding(new Insets(0,0,0,0));
-		hBox.getChildren().add(new ImageView("image/" + list.get(0) + ".png"));
+		/*
+		hBox.getChildren().add(new ImageView("image/" + list.get(10) + ".png"));
+		hBox.getChildren().add(new ImageView("image/" + list.get(11) + ".png"));
 		hBox.getChildren().add(new ImageView("image/" + list.get(1) + ".png"));
-		hBox.getChildren().add(new ImageView("image/" + list.get(2) + ".png"));
+		*/
+		hBox.getChildren().add(new ImageView("image/1.png"));
+		hBox.getChildren().add(new ImageView("image/11.png"));
+		hBox.getChildren().add(new ImageView("image/1.png"));
+		
 		
 		// Create buttons
 		HBox hBtnBox = new HBox(10);
 		hBtnBox.setAlignment(Pos.CENTER);
 		hBtnBox.setPadding(new Insets(5,0,5,0));
+
 		
 		Button btSpin = new Button("Spin");
 		btSpin.setOnAction(e -> {
 			java.util.Collections.shuffle(list);
 			
 			hBox.getChildren().clear();
-			hBox.getChildren().add(new ImageView("image/" + list.get(0) + ".png"));
-			hBox.getChildren().add(new ImageView("image/" + list.get(1) + ".png"));
+			
+			/*
+			hBox.getChildren().add(new ImageView("image/" + list.get(13) + ".png"));
+			hBox.getChildren().add(new ImageView("image/" + list.get(12) + ".png"));
 			hBox.getChildren().add(new ImageView("image/" + list.get(2) + ".png"));
+			
+			hBox.getChildren().add(new ImageView("image/" + list.get(10) + ".png"));
+			hBox.getChildren().add(new ImageView("image/" + list.get(11) + ".png"));
+			hBox.getChildren().add(new ImageView("image/" + list.get(1) + ".png"));
+			*/
+			
+			
+//			this is where the points calculator should go
 
 		});
 		
 		Button btAddMoney = new Button("Play Again");
-		
-//		
-//		Button btSort = new Button("Sort");
-//		btSort.setOnAction(e -> {
-//			java.util.Collections.sort(list.subList(0, 5));
-			
-//			hBox.getChildren().clear();
-//			hBox.getChildren().add(new ImageView("image/card/" + list.get(0) + ".png"));
-//			hBox.getChildren().add(new ImageView("image/card/" + list.get(1) + ".png"));
-//			hBox.getChildren().add(new ImageView("image/card/" + list.get(2) + ".png"));
-//			hBox.getChildren().add(new ImageView("image/card/" + list.get(3) + ".png"));
-//			hBox.getChildren().add(new ImageView("image/card/" + list.get(4) + ".png"));
-//		});
 		
 		hBtnBox.getChildren().addAll(btSpin, btAddMoney);
 		
